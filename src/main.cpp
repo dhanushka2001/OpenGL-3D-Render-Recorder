71efc90fe58be2ab73f57fa3ee6eebc60272edf9
dhanushka2001 <dhanushka2001@gmail.com>
2024-08-30 17:11:29 +0100

    Add off-screen rendering

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,17 @@
 # define M_PI           3.14159265358979323846
 #include <cmath>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+#include <vector>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 float redValue(float timeValue);
@@ -15,10 +26,16 @@ float blueValue(float timeValue);
 float greenValue(float timeValue);
 float xRotate(float r, float theta, float timeValue);
 float yRotate(float r, float theta, float timeValue);
+void saveImage(char* filepath, GLFWwindow* window);
 
 // settings
 const unsigned int SCR_WIDTH = 1080;
 const unsigned int SCR_HEIGHT = 1080;
+const unsigned int CHANNEL_COUNT = 3;
+
+const int DATA_SIZE = SCR_WIDTH * SCR_HEIGHT * CHANNEL_COUNT;
+const int PBO_COUNT = 2;
+GLuint pboIds[PBO_COUNT];
 
 int main()
 {
@@ -90,6 +107,35 @@ int main()
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     // glBindVertexArray(0);
 
+    int frame = 0;
+
+    int index = 0;
+    int nextIndex = 1;
+    // int index_stride = 0;
+
+    // create 2 pixel buffer objects, you need to delete them when program exits.
+    // glBufferData() with NULL pointer reserves only memory space.
+    glGenBuffers(PBO_COUNT, pboIds);
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[0]);
+    glBufferData(GL_PIXEL_PACK_BUFFER, DATA_SIZE, 0, GL_DYNAMIC_READ);
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[1]);
+    glBufferData(GL_PIXEL_PACK_BUFFER, DATA_SIZE, 0, GL_DYNAMIC_READ);
+
+    glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+
+    //Somewhere at initialization
+    /*  Framebuffer */
+    GLuint fbo, render_buf;
+    glGenFramebuffers(1,&fbo);
+    glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
+
+    /* Color renderbuffer. */
+    glGenRenderbuffers(1,&render_buf);
+    glBindRenderbuffer(GL_RENDERBUFFER, render_buf);
+    /* Storage must be one of: */
+    /* GL_RGBA4, GL_RGB565, GL_RGB5_A1, GL_DEPTH_COMPONENT16, GL_STENCIL_INDEX8. */
+    glRenderbufferStorage(GL_RENDERBUFFER, GL_RGBA4, SCR_WIDTH, SCR_HEIGHT);
+    glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, render_buf);
 
     // render loop
     // -----------
@@ -98,6 +144,9 @@ int main()
         // input
         // -----
         processInput(window);
+
+        //Before drawing
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
 
         // render
         // ------
@@ -123,6 +172,64 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
+
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+
+        char filepath[256];
+        sprintf(filepath, "../images/output/frame%03d.png", frame+1);
+        // saveImage(filepath, window);
+        frame++;
+
+        // "index" is used to read pixels from framebuffer to a PBO
+        // "nextIndex" is used to update pixels in the other PBO
+        // index = (index + 1) % PBO_COUNT;
+        // nextIndex = (index + 1) % PBO_COUNT;
+        GLsizei stride = CHANNEL_COUNT * SCR_WIDTH;
+        //stride += (stride % 4) ? (4 - stride % 4) : 0;
+        //glPixelStorei(GL_PACK_ALIGNMENT, 4);
+
+        // set the target framebuffer to read
+        /* GL_BACK, GL_READ_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 */
+        glReadBuffer(GL_READ_FRAMEBUFFER);
+
+        // read pixels from framebuffer to PBO
+        // glReadPixels() should return immediately.
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[index]);
+        glReadPixels(0, 0, SCR_WIDTH, SCR_HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+        // map the PBO to process its data by CPU
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[nextIndex]);
+        GLubyte* ptr = (GLubyte*)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+        if(ptr)
+        {
+            stbi_flip_vertically_on_write(true);
+            stbi_write_png(filepath, SCR_WIDTH, SCR_HEIGHT, CHANNEL_COUNT, ptr, stride);
+            glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+        }
+
+        // if (index == 4)
+        // {
+        //     for (int i = 0; i<5; i++)
+        //     {
+        //         sprintf(filepath, "../images/output/frame%03d.png", i + PBO_COUNT*index_stride);
+        //         glBindBuffer(GL_PIXEL_PACK_BUFFER, pboIds[i]);
+        //         GLubyte* ptr = (GLubyte*)glMapBuffer(GL_PIXEL_PACK_BUFFER, GL_READ_ONLY);
+        //         stbi_flip_vertically_on_write(true);
+        //         stbi_write_png(filepath, SCR_WIDTH, SCR_HEIGHT, CHANNEL_COUNT, ptr, stride);
+        //         glUnmapBuffer(GL_PIXEL_PACK_BUFFER);
+        //     }
+
+        //     index_stride++;
+        // }
+        index = (index + 1) % PBO_COUNT;
+        nextIndex = (nextIndex + 1) % PBO_COUNT;
+
+        // back to conventional pixel operation
+        glBindBuffer(GL_PIXEL_PACK_BUFFER, 0);
+        glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
+
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
@@ -133,6 +240,10 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(PBO_COUNT, pboIds);
+    //At deinit:
+    glDeleteFramebuffers(1,&fbo);
+    glDeleteRenderbuffers(1,&render_buf);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
@@ -189,4 +300,20 @@ float yRotate(float r, float theta, float timeValue)
     float y = r*sin(theta);
     float yRotate = x*sin(timeValue) + y*cos(timeValue);
     return yRotate;
+}
+
+// https://lencerf.github.io/post/2019-09-21-save-the-opengl-rendering-to-image-file/#:~:text=Quick%20answer,data%20to%20file%20using%20stbi_write_png%20.&text=I%20used%20GLFW%20to%20create,image%20data%20to%20png%20files.
+void saveImage(char* filepath, GLFWwindow* window) {
+ int width, height;
+ glfwGetFramebufferSize(window, &width, &height);
+ GLsizei nrChannels = 3;
+ GLsizei stride = nrChannels * width;
+ stride += (stride % 4) ? (4 - stride % 4) : 0;
+ GLsizei bufferSize = stride * height;
+ std::vector<char> buffer(bufferSize);
+ glPixelStorei(GL_PACK_ALIGNMENT, 4);
+ glReadBuffer(GL_FRONT);
+ glReadPixels(0, 0, width, height, GL_RGB, GL_UNSIGNED_BYTE, buffer.data());
+ stbi_flip_vertically_on_write(true);
+ stbi_write_png(filepath, width, height, nrChannels, buffer.data(), stride);
 }
